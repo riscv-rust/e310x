e6ee06a7b2c19e1180bf5bd735a7d2d9b5097311
Vadim Kaushan <admin@disasm.info>
2019-02-26 12:06:26 +0300

    Fix build
    Corresponding changes in svd2rust:
    https://github.com/rust-embedded/svd2rust/pull/279/commits/e2e8407558135ecab34d0043408e177f2d94dd66
    https://github.com/rust-embedded/svd2rust/pull/284/commits/ac2f6875bee921a33a7686457dbb156f8ad97175 (better fix)

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,4 @@
-#![cfg_attr(feature = "rt", feature(global_asm))]
-#![cfg_attr(feature = "rt", feature(use_extern_macros))]
-#![cfg_attr(feature = "rt", feature(used))]
 #![doc = "Peripheral access API for FE310 microcontrollers (generated using svd2rust v0.14.0)\n\nYou can find an overview of the API [here].\n\n[here]: https://docs.rs/svd2rust/0.14.0/svd2rust/#peripheral-api"]
-#![deny(missing_docs)]
 #![deny(warnings)]
 #![allow(non_camel_case_types)]
 #![no_std]
