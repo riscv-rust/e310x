5bfb8b7fa0d4beb57e28166e0700e24603908312
Vadim Kaushan <admin@disasm.info>
2019-03-15 15:54:07 +0300

    Remove structs from prelude

--- src/prelude.rs
+++ src/prelude.rs
@@ -2,12 +2,10 @@
 
 pub use embedded_hal::prelude::*;
 pub use crate::clint::ClintExt as _e310x_hal_clint_ClintExt;
-pub use crate::clock::Clocks;
 pub use crate::clock::PrciExt as _e310x_hal_clock_PrciExt;
 pub use crate::clock::AonExt as _e310x_hal_clock_AonExt;
 pub use crate::gpio::GpioExt as _e310x_hal_gpio_GpioExt;
 pub use crate::plic::PlicExt as _e310x_hal_plic_PlicExt;
 pub use crate::rtc::RtcExt as _e310x_hal_rtc_RtcExt;
-pub use crate::serial::{Serial, Tx, Rx};
 pub use crate::time::U32Ext as _e310x_hal_time_U32Ext;
 pub use crate::wdog::WdogExt as _e310x_hal_wdog_WdogExt;
