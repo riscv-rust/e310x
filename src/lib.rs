98f1fe0299a3d250a6cd865f0faa880518c40418
Vadim Kaushan <admin@disasm.info>
2019-05-18 20:54:06 +0300

    Remove #![deny(warnings)] (anti-pattern)

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 #![doc = "Peripheral access API for FE310 microcontrollers (generated using svd2rust v0.14.0)\n\nYou can find an overview of the API [here].\n\n[here]: https://docs.rs/svd2rust/0.14.0/svd2rust/#peripheral-api"]
 #![deny(missing_docs)]
-#![deny(warnings)]
 #![allow(non_camel_case_types)]
 #![no_std]
 extern crate bare_metal;
