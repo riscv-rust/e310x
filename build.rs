564e553fc8f0f78a2b6e4a1f9067202948384e01
Vadim Kaushan <admin@disasm.info>
2019-05-18 19:02:32 +0300

    Regenerate library code

--- build.rs
+++ build.rs
@@ -1,13 +1,15 @@
-use std::{env, fs};
-use std::path::PathBuf;
 use std::io::Write;
+use std::path::PathBuf;
+use std::{env, fs};
 
 fn main() {
     // Put the linker script somewhere the linker can find it
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
     println!("cargo:rustc-link-search={}", out_dir.display());
 
-    fs::File::create(out_dir.join("memory-fe310.x")).unwrap()
-        .write_all(include_bytes!("memory-fe310.x")).unwrap();
+    fs::File::create(out_dir.join("memory-fe310.x"))
+        .unwrap()
+        .write_all(include_bytes!("memory-fe310.x"))
+        .unwrap();
     println!("cargo:rerun-if-changed=memory-fe310.x");
 }
