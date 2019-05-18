c375fb46630d3445340b78b97f03a91dbf3e43bf
Vadim Kaushan <admin@disasm.info>
2019-05-18 19:02:02 +0300

    Generate library code as module

--- update.sh
+++ update.sh
@@ -2,9 +2,17 @@
 set -x
 set -e
 
-rm -rf src
-mkdir src
+rm -rf src/common
+mkdir src/common
 svd2rust --target riscv -i e310x.svd
-form -i lib.rs -o src
+form -i lib.rs -o src/common
 rm lib.rs
+
+# Convert library into module
+mv src/common/lib.rs src/common/mod.rs
 cargo fmt
+rustfmt src/common/mod.rs
+
+# Strip crate-level things
+tail -n+6 src/common/mod.rs > src/common/_mod.rs
+mv src/common/_mod.rs src/common/mod.rs
