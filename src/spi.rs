9f6d58ae726eb515df84b4bbfe8faedb805951d0
Vadim Kaushan <admin@disasm.info>
2019-06-15 12:57:41 +0300

    Get rid of macro generation in SPI driver

--- src/spi.rs
+++ src/spi.rs
@@ -24,8 +24,9 @@
 //! - Interrupt::QSPI0
 
 use core::convert::Infallible;
+use core::ops::Deref;
 pub use embedded_hal::spi::{Mode, Phase, Polarity, MODE_0, MODE_1, MODE_2, MODE_3};
-use crate::e310x::{QSPI0, QSPI1, QSPI2};
+use crate::e310x::{QSPI0, QSPI1, QSPI2, qspi0};
 use crate::gpio::{IOF0, gpio0};
 use crate::clock::Clocks;
 use crate::time::Hertz;
@@ -75,147 +76,173 @@ impl<T> Pins<QSPI2> for (gpio0::Pin27<IOF0<T>>, gpio0::Pin28<IOF0<T>>, gpio0::Pi
     const CS_INDEX: Option<u32> = Some(0);
 }
 
+
+#[doc(hidden)]
+pub trait SpiX: Deref<Target = qspi0::RegisterBlock> {}
+impl SpiX for QSPI0 {}
+impl SpiX for QSPI1 {}
+impl SpiX for QSPI2 {}
+
+
 /// SPI abstraction
 pub struct Spi<SPI, PINS> {
     spi: SPI,
     pins: PINS,
 }
 
-macro_rules! hal {
-    ($($SPIX:ident: $spiX:ident,)+) => {
-        $(
-            impl <PINS> Spi<$SPIX, PINS> {
-                /// Configures the SPI peripheral to operate in full duplex master mode
-                pub fn $spiX(
-                    spi: $SPIX,
-                    pins: PINS,
-                    mode: Mode,
-                    freq: Hertz,
-                    clocks: Clocks,
-                ) -> Self
-                where
-                    PINS: Pins<$SPIX>,
-                {
-                    let div = clocks.tlclk().0 / (2 * freq.0) - 1;
-                    spi.div.write(|w| unsafe { w.bits(div) });
+impl<SPI: SpiX, PINS> Spi<SPI, PINS> {
+    /// Configures the SPI peripheral to operate in full duplex master mode
+    pub fn new(spi: SPI, pins: PINS, mode: Mode, freq: Hertz, clocks: Clocks) -> Self
+    where
+        PINS: Pins<SPI>
+    {
+        let div = clocks.tlclk().0 / (2 * freq.0) - 1;
+        spi.div.write(|w| unsafe { w.bits(div) });
 
-                    let cs_mode = if let Some(cs_index) = PINS::CS_INDEX {
-                        spi.csid.write(|w| unsafe { w.bits(cs_index) });
+        let cs_mode = if let Some(cs_index) = PINS::CS_INDEX {
+            spi.csid.write(|w| unsafe { w.bits(cs_index) });
 
-                        0 // AUTO: Assert/de-assert CS at the beginning/end of each frame
-                    } else {
-                        3 // OFF: Disable hardware control of the CS pin
-                    };
-                    spi.csmode.write(|w| unsafe { w.bits(cs_mode) });
+            0 // AUTO: Assert/de-assert CS at the beginning/end of each frame
+        } else {
+            3 // OFF: Disable hardware control of the CS pin
+        };
+        spi.csmode.write(|w| unsafe { w.bits(cs_mode) });
 
-                    // Set CS pin polarity to high
-                    spi.csdef.reset();
+        // Set CS pin polarity to high
+        spi.csdef.reset();
 
-                    // Set SPI mode
-                    let phase = mode.phase == Phase::CaptureOnSecondTransition;
-                    let polarity = mode.polarity == Polarity::IdleHigh;
-                    spi.mode.write(|w| w
-                        .phase().bit(phase)
-                        .polarity().bit(polarity)
-                    );
+        // Set SPI mode
+        let phase = mode.phase == Phase::CaptureOnSecondTransition;
+        let polarity = mode.polarity == Polarity::IdleHigh;
+        spi.mode.write(|w| w
+            .phase().bit(phase)
+            .polarity().bit(polarity)
+        );
 
-                    spi.fmt.write(|w| unsafe { w
-                        .protocol().bits(0) // Single
-                        .endian().clear_bit() // Transmit most-significant bit (MSB) first
-                        .direction().rx()
-                        .length().bits(8)
-                    });
+        spi.fmt.write(|w| unsafe { w
+            .protocol().bits(0) // Single
+            .endian().clear_bit() // Transmit most-significant bit (MSB) first
+            .direction().rx()
+            .length().bits(8)
+        });
 
-                    // Set watermark levels
-                    spi.txmark.write(|w| unsafe { w.value().bits(1) });
-                    spi.rxmark.write(|w| unsafe { w.value().bits(0) });
+        // Set watermark levels
+        spi.txmark.write(|w| unsafe { w.value().bits(1) });
+        spi.rxmark.write(|w| unsafe { w.value().bits(0) });
 
-                    spi.delay0.reset();
-                    spi.delay1.reset();
+        spi.delay0.reset();
+        spi.delay1.reset();
 
-                    Self { spi, pins }
-                }
+        Self { spi, pins }
+    }
 
-                /// Sets transmit watermark level
-                pub fn set_tx_watermark(&mut self, value: u8) {
-                    self.spi.txmark.write(|w| unsafe { w.value().bits(value) });
-                }
+    /// Sets transmit watermark level
+    pub fn set_tx_watermark(&mut self, value: u8) {
+        self.spi.txmark.write(|w| unsafe { w.value().bits(value) });
+    }
 
-                /// Sets receive watermark level
-                pub fn set_rx_watermark(&mut self, value: u8) {
-                    self.spi.rxmark.write(|w| unsafe { w.value().bits(value) });
-                }
+    /// Sets receive watermark level
+    pub fn set_rx_watermark(&mut self, value: u8) {
+        self.spi.rxmark.write(|w| unsafe { w.value().bits(value) });
+    }
 
-                /// Returns transmit watermark event status
-                pub fn tx_wm_is_pending(&self) -> bool {
-                    self.spi.ip.read().txwm().bit()
-                }
+    /// Returns transmit watermark event status
+    pub fn tx_wm_is_pending(&self) -> bool {
+        self.spi.ip.read().txwm().bit()
+    }
 
-                /// Returns receive watermark event status
-                pub fn rx_wm_is_pending(&self) -> bool {
-                    self.spi.ip.read().rxwm().bit()
-                }
+    /// Returns receive watermark event status
+    pub fn rx_wm_is_pending(&self) -> bool {
+        self.spi.ip.read().rxwm().bit()
+    }
 
-                /// Starts listening for transmit watermark interrupt event
-                pub fn listen_tx_wm(&mut self) {
-                    self.spi.ie.write(|w| w.txwm().set_bit())
-                }
+    /// Starts listening for transmit watermark interrupt event
+    pub fn listen_tx_wm(&mut self) {
+        self.spi.ie.write(|w| w.txwm().set_bit())
+    }
 
-                /// Starts listening for receive watermark interrupt event
-                pub fn listen_rx_wm(&mut self) {
-                    self.spi.ie.write(|w| w.rxwm().set_bit())
-                }
+    /// Starts listening for receive watermark interrupt event
+    pub fn listen_rx_wm(&mut self) {
+        self.spi.ie.write(|w| w.rxwm().set_bit())
+    }
 
-                /// Stops listening for transmit watermark interrupt event
-                pub fn unlisten_tx_wm(&mut self) {
-                    self.spi.ie.write(|w| w.txwm().clear_bit())
-                }
+    /// Stops listening for transmit watermark interrupt event
+    pub fn unlisten_tx_wm(&mut self) {
+        self.spi.ie.write(|w| w.txwm().clear_bit())
+    }
 
-                /// Stops listening for receive watermark interrupt event
-                pub fn unlisten_rx_wm(&mut self) {
-                    self.spi.ie.write(|w| w.rxwm().clear_bit())
-                }
+    /// Stops listening for receive watermark interrupt event
+    pub fn unlisten_rx_wm(&mut self) {
+        self.spi.ie.write(|w| w.rxwm().clear_bit())
+    }
 
-                /// Releases the SPI peripheral and associated pins
-                pub fn free(self) -> ($SPIX, PINS) {
-                    (self.spi, self.pins)
-                }
-            }
-
-            impl<PINS> embedded_hal::spi::FullDuplex<u8> for Spi<$SPIX, PINS> {
-                type Error = Infallible;
-
-                fn read(&mut self) -> nb::Result<u8, Infallible> {
-                    let rxdata = self.spi.rxdata.read();
-
-                    if rxdata.empty().bit_is_set() {
-                        Err(nb::Error::WouldBlock)
-                    } else {
-                        Ok(rxdata.data().bits())
-                    }
-                }
-
-                fn send(&mut self, byte: u8) -> nb::Result<(), Infallible> {
-                    let txdata = self.spi.txdata.read();
-
-                    if txdata.full().bit_is_set() {
-                        Err(nb::Error::WouldBlock)
-                    } else {
-                        self.spi.txdata.write(|w| unsafe { w.data().bits(byte) });
-                        Ok(())
-                    }
-                }
-            }
-
-            impl<PINS> embedded_hal::blocking::spi::transfer::Default<u8> for Spi<$SPIX, PINS> {}
-
-            impl<PINS> embedded_hal::blocking::spi::write::Default<u8> for Spi<$SPIX, PINS> {}
-        )+
+    /// Releases the SPI peripheral and associated pins
+    pub fn free(self) -> (SPI, PINS) {
+        (self.spi, self.pins)
     }
 }
 
-hal! {
-    QSPI0: spi0,
-    QSPI1: spi1,
-    QSPI2: spi2,
+impl<SPI: SpiX, PINS> embedded_hal::spi::FullDuplex<u8> for Spi<SPI, PINS> {
+    type Error = Infallible;
+
+    fn read(&mut self) -> nb::Result<u8, Infallible> {
+        let rxdata = self.spi.rxdata.read();
+
+        if rxdata.empty().bit_is_set() {
+            Err(nb::Error::WouldBlock)
+        } else {
+            Ok(rxdata.data().bits())
+        }
+    }
+
+    fn send(&mut self, byte: u8) -> nb::Result<(), Infallible> {
+        let txdata = self.spi.txdata.read();
+
+        if txdata.full().bit_is_set() {
+            Err(nb::Error::WouldBlock)
+        } else {
+            self.spi.txdata.write(|w| unsafe { w.data().bits(byte) });
+            Ok(())
+        }
+    }
+}
+
+impl<SPI: SpiX, PINS> embedded_hal::blocking::spi::transfer::Default<u8> for Spi<SPI, PINS> {}
+
+impl<SPI: SpiX, PINS> embedded_hal::blocking::spi::write::Default<u8> for Spi<SPI, PINS> {}
+
+
+
+// Backward compatibility
+impl<PINS> Spi<QSPI0, PINS> {
+    /// Configures the SPI peripheral to operate in full duplex master mode
+    #[deprecated(note = "Please use Spi::new function instead")]
+    pub fn spi0(spi: QSPI0, pins: PINS, mode: Mode, freq: Hertz, clocks: Clocks) -> Self
+    where
+        PINS: Pins<QSPI0>
+    {
+        Self::new(spi, pins, mode, freq, clocks)
+    }
+}
+
+impl<PINS> Spi<QSPI1, PINS> {
+    /// Configures the SPI peripheral to operate in full duplex master mode
+    #[deprecated(note = "Please use Spi::new function instead")]
+    pub fn spi1(spi: QSPI1, pins: PINS, mode: Mode, freq: Hertz, clocks: Clocks) -> Self
+        where
+            PINS: Pins<QSPI1>
+    {
+        Self::new(spi, pins, mode, freq, clocks)
+    }
+}
+
+impl<PINS> Spi<QSPI2, PINS> {
+    /// Configures the SPI peripheral to operate in full duplex master mode
+    #[deprecated(note = "Please use Spi::new function instead")]
+    pub fn spi2(spi: QSPI2, pins: PINS, mode: Mode, freq: Hertz, clocks: Clocks) -> Self
+        where
+            PINS: Pins<QSPI2>
+    {
+        Self::new(spi, pins, mode, freq, clocks)
+    }
 }
