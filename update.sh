4fd8de6080114c40efbb3a92a11f24397258b81c
Vadim Kaushan <admin@disasm.info>
2020-02-04 00:22:03 +0300

    Fix update script

--- update.sh
+++ update.sh
@@ -14,7 +14,7 @@ cargo fmt
 rustfmt src/common/mod.rs
 
 # Strip crate-level things
-tail -n+20 src/common/mod.rs > src/common/_mod.rs
+tail -n+38 src/common/mod.rs > src/common/_mod.rs
 echo -en "use core::marker::PhantomData;\nuse core::ops::Deref;\n" > src/common/mod.rs
 cat src/common/_mod.rs >> src/common/mod.rs
 rm src/common/_mod.rs
