320995418bd79c9b2e9f72cbc1eb4da400752cef
Ales Katona <ales@katona.me>
2021-07-10 22:38:09 -0700

    implement DelayUs for Delay

--- src/delay.rs
+++ src/delay.rs
@@ -1,12 +1,14 @@
 //! # Delays
 
-use embedded_hal::blocking::delay::DelayMs;
-use crate::core::clint::{MTIME, MTIMECMP};
 use crate::clock::Clocks;
+use crate::core::clint::{MTIME, MTIMECMP};
+use embedded_hal::blocking::delay::{DelayMs, DelayUs};
 use riscv::register::{mie, mip};
 
 /// Machine timer (mtime) as a busyloop delay provider
 pub struct Delay;
+
+const TICKS_PER_SECOND: u64 = 32768;
 
 impl Delay {
     /// Constructs a delay provider based on the machine timer (mtime)
@@ -15,13 +17,31 @@ impl Delay {
     }
 }
 
-impl DelayMs<u32> for Delay {
-    fn delay_ms(&mut self, ms: u32) {
-        let ticks = (ms as u64) * 32768 / 1000;
+impl DelayUs<u32> for Delay {
+    fn delay_us(&mut self, us: u32) {
+        let ticks = (us as u64) * TICKS_PER_SECOND / 1000_0000;
 
         let mtime = MTIME;
         let t = mtime.mtime() + ticks;
-        while mtime.mtime() < t { }
+        while mtime.mtime() < t {}
+    }
+}
+
+impl DelayUs<u16> for Delay {
+    fn delay_us(&mut self, us: u16) {
+        self.delay_ms(u32::from(us));
+    }
+}
+
+impl DelayUs<u8> for Delay {
+    fn delay_us(&mut self, us: u8) {
+        self.delay_ms(u32::from(us));
+    }
+}
+
+impl DelayMs<u32> for Delay {
+    fn delay_ms(&mut self, ms: u32) {
+        self.delay_us(ms * 1000);
     }
 }
 
@@ -40,7 +60,7 @@ impl DelayMs<u8> for Delay {
 /// Machine timer (mtime) as a sleep delay provider using mtimecmp
 pub struct Sleep {
     clock_freq: u32,
-    mtimecmp: MTIMECMP
+    mtimecmp: MTIMECMP,
 }
 
 impl Sleep {
@@ -64,7 +84,7 @@ impl DelayMs<u32> for Sleep {
         unsafe {
             mie::set_mtimer();
         }
-        
+
         // Wait For Interrupt will put CPU to sleep until an interrupt hits
         // in our case when internal timer mtime value >= mtimecmp value
         // after which empty handler gets called and we go into the
