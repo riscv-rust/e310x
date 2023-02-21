fb581299e9543f28298de8d525dca77c8af96c62
Daniel Pérez <steew0x8@protonmail.com>
2023-02-21 14:41:15 +0100

    rename virq linker script

--- build.rs
+++ build.rs
@@ -5,6 +5,6 @@ fn main() {
     // Put the linker script somewhere the linker can find it
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
     println!("cargo:rustc-link-search={}", out_dir.display());
-    fs::copy("interrupts.x", out_dir.join("interrupts.x")).unwrap();
-    println!("cargo:rerun-if-changed=interrupts.x");
+    fs::copy("fe310x-interrupt.x", out_dir.join("fe310x-interrupt.x")).unwrap();
+    println!("cargo:rerun-if-changed=fe310x-interrupt.x");
 }
