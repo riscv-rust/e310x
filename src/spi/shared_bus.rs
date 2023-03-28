230c44aa7e0787ec52020d629459352c98acacd9
Ales Katona <ales@katona.me>
2023-03-28 13:35:50 -0700

    update e310x dependency to v0.11.0

--- src/spi/shared_bus.rs
+++ src/spi/shared_bus.rs
@@ -2,13 +2,12 @@ use core::cell::RefCell;
 use core::ops::Deref;
 pub use embedded_hal::spi::{Mode, Phase, Polarity, MODE_0, MODE_1, MODE_2, MODE_3};
 use riscv::interrupt;
-use riscv::interrupt::Mutex;
 
 use super::{PinCS, PinsNoCS, SpiBus, SpiConfig, SpiSharedDevice, SpiX};
 
 /// Newtype for RefCell<Spi> locked behind a Mutex.
 /// Used to hold the [SpiBus] instance so it can be used for multiple [SpiSharedDevice] instances.
-pub struct SharedBus<SPI, PINS>(Mutex<RefCell<SpiBus<SPI, PINS>>>);
+pub struct SharedBus<SPI, PINS>(RefCell<SpiBus<SPI, PINS>>);
 
 impl<SPI, PINS> SharedBus<SPI, PINS>
 where
@@ -16,7 +15,7 @@ where
     PINS: PinsNoCS<SPI>,
 {
     pub(crate) fn new(bus: SpiBus<SPI, PINS>) -> Self {
-        Self(Mutex::new(RefCell::new(bus)))
+        Self(RefCell::new(bus))
     }
 
     /// Create a new shared device on this SPI bus.
@@ -39,30 +38,30 @@ where
 {
     /// Set HOLD CS mode to per-frame operation, unless CSMODE is set to OFF
     pub fn start_frame(&mut self) {
-        interrupt::free(|cs| {
-            let mut bus = self.0.borrow(*cs).borrow_mut();
+        interrupt::free(|| {
+            let mut bus = self.0.borrow_mut();
             bus.start_frame();
         });
     }
 
     /// Finishes transfer by deasserting CS (only for hardware-controlled CS)
     pub fn end_frame(&mut self) {
-        interrupt::free(|cs| {
-            let mut bus = self.0.borrow(*cs).borrow_mut();
+        interrupt::free(|| {
+            let mut bus = self.0.borrow_mut();
             bus.end_frame();
         });
     }
 
     /// Releases the SPI peripheral and associated pins
     pub fn release(self) -> (SPI, PINS) {
-        let bus = self.0.into_inner().into_inner();
+        let bus = self.0.into_inner();
 
         (bus.spi, bus.pins)
     }
 }
 
 impl<SPI, PINS> Deref for SharedBus<SPI, PINS> {
-    type Target = Mutex<RefCell<SpiBus<SPI, PINS>>>;
+    type Target = RefCell<SpiBus<SPI, PINS>>;
 
     fn deref(&self) -> &Self::Target {
         &self.0
