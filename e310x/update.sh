67907eab4137603f3af145708b1e42c6bd82e8d6
Román Cárdenas Rodríguez <rcardenas.rod@gmail.com>
2025-02-08 12:21:45 +0100

    mtvec_alignment in configuration

--- e310x/update.sh
+++ e310x/update.sh
@@ -5,7 +5,8 @@ set -e
 # used svd2rust 0.34.0
 rm -rf src
 mkdir src
-svd2rust --target riscv --settings settings.yaml -g -i e310x.svd
+../../svd2rust/target/release/svd2rust --target riscv --settings settings.yaml -g -i e310x.svd
+# svd2rust --target riscv --settings settings.yaml -g -i e310x.svd
 mv generic.rs src/
 form -i lib.rs -o src/
 rm lib.rs
