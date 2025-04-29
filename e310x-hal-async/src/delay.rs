639637b72f5c45d4316087157cc3fa30627394b9
dmunizu <d.muniz@alumnos.upm.es>
2025-04-29 14:03:45 +0200

    Comments added for all 3 delay functions

--- e310x-hal-async/src/delay.rs
+++ e310x-hal-async/src/delay.rs
@@ -15,10 +15,10 @@ fn _riscv_peripheral_aclint_mtimer() -> MTIMER {
 
 /// Tries to push a new timer to the timer queue assigned to the `MTIMER` register for the current HART ID.
 /// If it fails (e.g., the timer queue is full), it returns back the timer that failed to be pushed.
-fn _riscv_peripheral_aclint_push_timer(t: Timer) -> Result<(), Timer>{
+fn _riscv_peripheral_aclint_push_timer(t: Timer) -> Result<(), Timer> {}
 
-}
-
-fn _riscv_peripheral_aclint_wake_timers(current_tick: u64) -> Option<u64>{
-
-}
+/// Pops all the expired timers from the timer queue assigned to the `MTIMER` register for the
+/// current HART ID and wakes their associated wakers. Once it is done, if the queue is empty,
+/// it returns `None`. Alternatively, if the queue is not empty but the earliest timer has not expired
+/// yet, it returns `Some(next_expires)` where `next_expires` is the tick at which this timer expires.
+fn _riscv_peripheral_aclint_wake_timers(current_tick: u64) -> Option<u64> {}
