27f17284b450c71b91a63ae15d723de6c30c5944
unknown <d.muniz@alumnos.upm.es>
2025-04-29 14:00:11 +0200

    _riscv_peripheral_aclint_mtimer() implementation

--- e310x-hal-async/src/delay.rs
+++ e310x-hal-async/src/delay.rs
@@ -1,6 +1,24 @@
 //! # Delays
+//! # Note
+//!
+//! Implementation of the asynchronous delay methods for the (A)CLINT peripheral.
 
 use crate::clock::Clocks;
 use e310x::CLINT;
 use embedded_hal_async::delay::DelayNs;
 use riscv::register::mip;
+
+/// Returns the `MTIMER` register for the current HART ID.
+fn _riscv_peripheral_aclint_mtimer() -> MTIMER {
+    CLINT::mtimer().mtime
+}
+
+/// Tries to push a new timer to the timer queue assigned to the `MTIMER` register for the current HART ID.
+/// If it fails (e.g., the timer queue is full), it returns back the timer that failed to be pushed.
+fn _riscv_peripheral_aclint_push_timer(t: Timer) -> Result<(), Timer>{
+
+}
+
+fn _riscv_peripheral_aclint_wake_timers(current_tick: u64) -> Option<u64>{
+
+}
