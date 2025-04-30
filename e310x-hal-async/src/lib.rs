5e92cf99ee4d8c97def9ab89686d61f2aee0de28
Daniel Muñiz <d.muniz@alumnos.upm.es>
2025-04-30 12:50:44 +0200

    Docs for lib.rs

--- e310x-hal-async/src/lib.rs
+++ e310x-hal-async/src/lib.rs
@@ -1,3 +1,7 @@
+//! Asynchronous HAL for the E310x family of microcontrollers
+//!
+//! This is an implementation of the [`embedded-hal-async`] traits for the E310x
+//! family of microcontrollers.
 #![deny(missing_docs)]
 #![no_std]
 
