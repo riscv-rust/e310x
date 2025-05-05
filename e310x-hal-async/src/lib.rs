7da635775da567320bd531683b65ee57185db152
Daniel Muñiz <d.muniz@alumnos.upm.es>
2025-05-05 13:03:21 +0200

    Temporal Macro for examples

--- e310x-hal-async/src/lib.rs
+++ e310x-hal-async/src/lib.rs
@@ -6,4 +6,8 @@
 #![deny(missing_docs)]
 #![no_std]
 
+//Temporal macro to allow for async_delays
+use e310x::interrupt;
+riscv_peripheral :: clint_codegen ! (base 0x2000000 , freq 32768 , mtimecmps [mtimecmp0 = (crate :: interrupt :: Hart :: H0 , "[0](crate::interrupt::Hart::H0)")] , msips [msip0 = (crate :: interrupt :: Hart :: H0 , "[0](crate::interrupt::Hart::H0)")] , async_delay ,);
+
 pub mod delay;
