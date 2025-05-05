5678b976ed79f94a949f4682b1c3f1eaa2638d9b
Daniel Muñiz <d.muniz@alumnos.upm.es>
2025-05-05 12:01:27 +0200

    Embassy Executor addition

--- hifive1-async-examples/examples/led_blink.rs
+++ hifive1-async-examples/examples/led_blink.rs
@@ -1,9 +1,11 @@
 //! Basic blinking LEDs example using mtime/mtimecmp registers for "sleep" in a loop.
 //! Blinks each led once and goes to the next one.
+//! This example uses synchronous UART and only tests asynchronous Delay.
 
 #![no_std]
 #![no_main]
 
+use embassy_executor::Executor;
 use hifive1::{
     clock,
     hal::{e310x::CLINT, prelude::*, DeviceResources},
@@ -29,17 +31,17 @@ fn main() -> ! {
         clocks,
     );
 
-    // get blue LED pin
-    let pin = pin!(pins, led_blue);
-    let mut led = pin.into_inverted_output();
-
-    // Get the sleep struct from CLINT
-    let mut sleep = CLINT::delay();
-
-    const STEP: u32 = 1000; // 1s
-    loop {
-        Led::toggle(&mut led);
-        sprintln!("LED toggled. New state: {}", led.is_on());
-        sleep.delay_ms(STEP);
-    }
+    Executor::new().run(|spawner| async {
+        // get blue LED pin
+        let pin = pin!(pins, led_blue);
+        let mut led = pin.into_inverted_output();
+        // Get the sleep struct from CLINT
+        let mut sleep = CLINT::async_delay();
+        const STEP: u32 = 1000; // 1s
+        loop {
+            Led::toggle(&mut led);
+            sprintln!("LED toggled. New state: {}", led.is_on());
+            sleep.delay_ms(STEP).await;
+        }
+    });
 }
