9c6c0844b8429d13f0ef005f2cb01046d1700bf1
Daniel Muñiz <d.muniz@alumnos.upm.es>
2025-06-03 13:19:06 +0200

    hifive1-async-examples import

---
+++ hifive1-async-examples/examples/led_blink.rs
@@ -0,0 +1,72 @@
+//! Basic blinking LEDs example using mtime/mtimecmp registers for "sleep" in a loop.
+//! Blinks each led once and goes to the next one.
+//! This example uses synchronous UART and only tests asynchronous Delay.
+
+#![no_std]
+#![no_main]
+
+use e310x_hal_async::{CLINT, prelude::*};
+use embassy_executor::Executor;
+use hifive1::{
+    BLUE, Led, clock,
+    hal::{DeviceResources, prelude::*},
+    pin, sprintln,
+};
+use static_cell::StaticCell;
+extern crate panic_halt;
+
+static EXECUTOR: StaticCell<Executor> = StaticCell::new();
+
+#[embassy_executor::task]
+async fn blink(mut led: BLUE) {
+    // Get the sleep struct from CLINT
+    let mut sleep = CLINT::async_delay();
+    const STEP: u32 = 1000; // 1s
+    loop {
+        Led::toggle(&mut led);
+        let led_state = led.is_on();
+        sprintln!("LED toggled. New state: {}", led_state);
+
+        sleep.delay_ms(STEP).await;
+    }
+}
+
+#[riscv_rt::entry]
+fn main() -> ! {
+    let dr = DeviceResources::take().unwrap();
+    let p = dr.peripherals;
+    let pins: hifive1::hal::device::DeviceGpioPins = dr.pins;
+
+    // Configure clocks
+    let clocks = clock::configure(p.PRCI, p.AONCLK, 320.mhz().into());
+
+    //Blinking LED
+    let pin = pin!(pins, led_blue);
+    let led = pin.into_inverted_output();
+
+    // Configure UART for stdout
+    hifive1::stdout::configure(
+        p.UART0,
+        pin!(pins, uart0_tx),
+        pin!(pins, uart0_rx),
+        115_200.bps(),
+        clocks,
+    );
+
+    //Configure MTIMER interrupt
+    CLINT::mtimer_disable();
+    let mtimer = CLINT::mtimer();
+    let (mtimecmp, mtime) = (mtimer.mtimecmp0, mtimer.mtime);
+    mtime.write(0);
+    mtimecmp.write(2^16 - 1);
+    unsafe {
+        riscv::interrupt::enable();
+        CLINT::mtimer_enable();
+    }
+
+    //Execute task
+    let executor = EXECUTOR.init(Executor::new());
+    executor.run(|spawner| {
+        spawner.spawn(blink(led)).unwrap();
+    });
+}
