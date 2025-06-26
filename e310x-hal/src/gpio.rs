a385410a5cb4624807a624ff01b6ed3eabd7d9be
dmunizu <d.muniz@alumnos.upm.es>
2025-06-26 11:56:58 +0200

    Addition of individual pin methods

--- e310x-hal/src/gpio.rs
+++ e310x-hal/src/gpio.rs
@@ -4,6 +4,23 @@ use core::marker::PhantomData;
 
 use portable_atomic::{AtomicU32, Ordering};
 
+/// Event Type for GPIO interrupts.
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum EventType {
+    /// High level event
+    High,
+    /// Low level event
+    Low,
+    /// Rising edge event
+    Rise,
+    /// Falling edge event
+    Fall,
+    /// Both edges event
+    BothEdges,
+    /// All events
+    All,
+}
+
 /// GpioExt trait extends the GPIO0 peripheral.
 pub trait GpioExt {
     /// The parts to split the GPIO into.
@@ -141,7 +158,7 @@ trait PeripheralAccess {
 
 macro_rules! gpio {
     ($GPIOX:ident, $gpiox:ident, [
-        $($PXi:ident: ($pxi:ident, $i:expr, $MODE:ty),)+
+        $($PXi:ident: ($pxi:ident, $i:expr, $handle:ident, $MODE:ty),)+
     ]) => {
         /// GPIO
         pub mod $gpiox {
@@ -149,9 +166,9 @@ macro_rules! gpio {
             use core::convert::Infallible;
 
             use embedded_hal::digital::{InputPin, OutputPin, StatefulOutputPin, ErrorType};
-            use e310x::$GPIOX;
+            use e310x::{$GPIOX, Plic, interrupt::{ExternalInterrupt, Priority,},};
             use super::{Unknown, IOF0, IOF1, Drive, Floating, GpioExt, Input, Invert,
-                        NoInvert, Output, PullUp, Regular, PinIndex, PeripheralAccess};
+                        NoInvert, Output, PullUp, Regular, PinIndex, PeripheralAccess, EventType};
 
             /// GPIO parts for fine grained permission control.
             pub struct Parts {
@@ -276,6 +293,181 @@ macro_rules! gpio {
                         $GPIOX::set_iof_en(Self::INDEX, false);
                         $PXi { _mode: PhantomData }
                     }
+
+                    /// Enables the external interrupt source for the pin.
+                    /// # Safety
+                    ///
+                    /// Enabling an interrupt source can break mask-based critical sections.
+                    pub unsafe fn enable_exti() {
+                        let ctx = unsafe{Plic::steal()}.ctx0();
+                        ctx.enables().enable(ExternalInterrupt::$handle);
+                    }
+
+                    /// Disables the external interrupt source for the pin.
+                    pub fn disable_exti() {
+                        let ctx = unsafe{Plic::steal()}.ctx0();
+                        ctx.enables().disable(ExternalInterrupt::$handle);
+                    }
+
+                    /// Returns if the external interrupt source for the pin is enabled.
+                    pub fn is_exti_enabled() -> bool {
+                        let ctx = unsafe{Plic::steal()}.ctx0();
+                        ctx.enables().is_enabled(ExternalInterrupt::$handle)
+                    }
+
+                    /// Sets the external interrupt source priority.
+                    ///  # Safety
+                    ///
+                    ///  Changing the priority level can break priority-based critical sections.
+                    pub unsafe fn set_exti_priority(priority: Priority) {
+                        let priorities = unsafe{Plic::steal()}.priorities();
+                        priorities.set_priority(ExternalInterrupt::$handle, priority);
+                    }
+
+                    /// Returns the external interrupt source priority.
+                    pub fn get_exti_priority() -> Priority {
+                        let priorities = unsafe{Plic::steal()}.priorities();
+                        priorities.get_priority(ExternalInterrupt::$handle)
+                    }
+
+                    /// Enables the selected interrupts for the pin in the interrupt enable registers
+                    /// # Note
+                    ///
+                    ///  This function does not enable the interrupt in the PLIC, it only sets the
+                    /// interrupt enable bit in the GPIO peripheral. You must call
+                    /// [`enable_exti()`](Self::enable_exti) to enable the interrupt in
+                    /// the PLIC.
+                    pub fn enable_interrupt(event: EventType) {
+                        let gpio_block = $GPIOX::peripheral();
+                        let pin_mask = 1 << $i;
+
+                        unsafe{
+                            match event {
+                                EventType::High => {gpio_block.high_ie().modify(|r, w| w.bits(r.bits() | pin_mask));}
+                                EventType::Low => {gpio_block.low_ie().modify(|r, w| w.bits(r.bits() | pin_mask));}
+                                EventType::Rise => {gpio_block.rise_ie().modify(|r, w| w.bits(r.bits() | pin_mask));}
+                                EventType::Fall => {gpio_block.fall_ie().modify(|r, w| w.bits(r.bits() | pin_mask));}
+                                EventType::BothEdges => {
+                                    gpio_block.rise_ie().modify(|r, w| w.bits(r.bits() | pin_mask));
+                                    gpio_block.fall_ie().modify(|r, w| w.bits(r.bits() | pin_mask));
+                                }
+                                EventType::All => {
+                                    gpio_block.high_ie().modify(|r, w| w.bits(r.bits() | pin_mask));
+                                    gpio_block.low_ie().modify(|r, w| w.bits(r.bits() | pin_mask));
+                                    gpio_block.rise_ie().modify(|r, w| w.bits(r.bits() | pin_mask));
+                                    gpio_block.fall_ie().modify(|r, w| w.bits(r.bits() | pin_mask));
+                                }
+                            }
+                        }
+                    }
+
+                    /// Disables the selected interrupts for the pin in the interrupt enable registers
+                    pub fn disable_interrupt(event: EventType) {
+                        let gpio_block = $GPIOX::peripheral();
+                        let pin_mask = 1 << $i;
+
+                        unsafe{
+                            match event {
+                                EventType::High => {gpio_block.high_ie().modify(|r, w| w.bits(r.bits() & !pin_mask));}
+                                EventType::Low => {gpio_block.low_ie().modify(|r, w| w.bits(r.bits() & !pin_mask));}
+                                EventType::Rise => {gpio_block.rise_ie().modify(|r, w| w.bits(r.bits() & !pin_mask));}
+                                EventType::Fall => {gpio_block.fall_ie().modify(|r, w| w.bits(r.bits() & !pin_mask));}
+                                EventType::BothEdges => {
+                                    gpio_block.rise_ie().modify(|r, w| w.bits(r.bits() & !pin_mask));
+                                    gpio_block.fall_ie().modify(|r, w| w.bits(r.bits() & !pin_mask));
+                                }
+                                EventType::All => {
+                                    gpio_block.high_ie().modify(|r, w| w.bits(r.bits() & !pin_mask));
+                                    gpio_block.low_ie().modify(|r, w| w.bits(r.bits() & !pin_mask));
+                                    gpio_block.rise_ie().modify(|r, w| w.bits(r.bits() & !pin_mask));
+                                    gpio_block.fall_ie().modify(|r, w| w.bits(r.bits() & !pin_mask));
+                                }
+                            }
+                        }
+                    }
+
+                    /// Clears pending interrupts for the selected pin interrupts.
+                    pub fn clear_pending_interrupt(event: EventType) {
+                        let gpio_block = $GPIOX::peripheral();
+                        let pin_mask = 1 << $i;
+
+                        unsafe{
+                            match event {
+                                EventType::High => {gpio_block.high_ip().modify(|r, w| w.bits(r.bits() | pin_mask));}
+                                EventType::Low => {gpio_block.low_ip().modify(|r, w| w.bits(r.bits() | pin_mask));}
+                                EventType::Rise => {gpio_block.rise_ip().modify(|r, w| w.bits(r.bits() | pin_mask));}
+                                EventType::Fall => {gpio_block.fall_ip().modify(|r, w| w.bits(r.bits() | pin_mask));}
+                                EventType::BothEdges => {
+                                    gpio_block.rise_ip().modify(|r, w| w.bits(r.bits() | pin_mask));
+                                    gpio_block.fall_ip().modify(|r, w| w.bits(r.bits() | pin_mask));
+                                }
+                                EventType::All => {
+                                    gpio_block.high_ip().modify(|r, w| w.bits(r.bits() | pin_mask));
+                                    gpio_block.low_ip().modify(|r, w| w.bits(r.bits() | pin_mask));
+                                    gpio_block.rise_ip().modify(|r, w| w.bits(r.bits() | pin_mask));
+                                    gpio_block.fall_ip().modify(|r, w| w.bits(r.bits() | pin_mask));
+                                }
+                            }
+                        }
+                    }
+
+                    /// Returns true if the interrupt for the pin is enabled.
+                    ///  # Note
+                    ///
+                    ///  Both Edges will return true if either the
+                    ///  rising or falling edge interrupts are enabled
+                    ///  and All will return true if any of the
+                    ///  interrupts are enabled.
+                    pub fn is_interrupt_enabled(event: EventType) -> bool {
+                        let gpio_block = $GPIOX::peripheral();
+                        let pin_mask = 1 << $i;
+
+                        match event {
+                            EventType::High => gpio_block.high_ie().read().bits() & pin_mask != 0,
+                            EventType::Low => gpio_block.low_ie().read().bits() & pin_mask != 0,
+                            EventType::Rise => gpio_block.rise_ie().read().bits() & pin_mask != 0,
+                            EventType::Fall => gpio_block.fall_ie().read().bits() & pin_mask != 0,
+                            EventType::BothEdges => {
+                                (gpio_block.rise_ie().read().bits() & pin_mask != 0) ||
+                                (gpio_block.fall_ie().read().bits() & pin_mask != 0)
+                            }
+                            EventType::All => {
+                                (gpio_block.high_ie().read().bits() & pin_mask != 0) ||
+                                (gpio_block.low_ie().read().bits() & pin_mask != 0) ||
+                                (gpio_block.rise_ie().read().bits() & pin_mask != 0) ||
+                                (gpio_block.fall_ie().read().bits() & pin_mask != 0)
+                            }
+                        }
+                    }
+
+                    /// Returns true if the interrupt for the pin is pending.
+                    ///  # Note
+                    ///
+                    ///  Both Edges will return true if either the
+                    ///  rising or falling edge interrupts are pending
+                    ///  and All will return true if any of the
+                    ///  interrupts are pending.
+                    pub fn is_interrupt_pending(event: EventType) -> bool {
+                        let gpio_block = $GPIOX::peripheral();
+                        let pin_mask = 1 << $i;
+
+                        match event {
+                            EventType::High => gpio_block.high_ip().read().bits() & pin_mask != 0,
+                            EventType::Low => gpio_block.low_ip().read().bits() & pin_mask != 0,
+                            EventType::Rise => gpio_block.rise_ip().read().bits() & pin_mask != 0,
+                            EventType::Fall => gpio_block.fall_ip().read().bits() & pin_mask != 0,
+                            EventType::BothEdges => {
+                                (gpio_block.rise_ip().read().bits() & pin_mask != 0) ||
+                                (gpio_block.fall_ip().read().bits() & pin_mask != 0)
+                            }
+                            EventType::All => {
+                                (gpio_block.high_ip().read().bits() & pin_mask != 0) ||
+                                (gpio_block.low_ip().read().bits() & pin_mask != 0) ||
+                                (gpio_block.rise_ip().read().bits() & pin_mask != 0) ||
+                                (gpio_block.fall_ip().read().bits() & pin_mask != 0)
+                            }
+                        }
+                    }
                 }
 
                 #[cfg(not(feature = "async"))]
@@ -339,36 +531,36 @@ macro_rules! gpio {
 // * bootloader may reconfigure some GPIOs
 // * we do not enforce any specific state in `split()`
 gpio!(Gpio0, gpio0, [
-    Pin0: (pin0, 0, Unknown),
-    Pin1: (pin1, 1, Unknown),
-    Pin2: (pin2, 2, Unknown),
-    Pin3: (pin3, 3, Unknown),
-    Pin4: (pin4, 4, Unknown),
-    Pin5: (pin5, 5, Unknown),
-    Pin6: (pin6, 6, Unknown),
-    Pin7: (pin7, 7, Unknown),
-    Pin8: (pin8, 8, Unknown),
-    Pin9: (pin9, 9, Unknown),
-    Pin10: (pin10, 10, Unknown),
-    Pin11: (pin11, 11, Unknown),
-    Pin12: (pin12, 12, Unknown),
-    Pin13: (pin13, 13, Unknown),
-    Pin14: (pin14, 14, Unknown),
-    Pin15: (pin15, 15, Unknown),
-    Pin16: (pin16, 16, Unknown),
-    Pin17: (pin17, 17, Unknown),
-    Pin18: (pin18, 18, Unknown),
-    Pin19: (pin19, 19, Unknown),
-    Pin20: (pin20, 20, Unknown),
-    Pin21: (pin21, 21, Unknown),
-    Pin22: (pin22, 22, Unknown),
-    Pin23: (pin23, 23, Unknown),
-    Pin24: (pin24, 24, Unknown),
-    Pin25: (pin25, 25, Unknown),
-    Pin26: (pin26, 26, Unknown),
-    Pin27: (pin27, 27, Unknown),
-    Pin28: (pin28, 28, Unknown),
-    Pin29: (pin29, 29, Unknown),
-    Pin30: (pin30, 30, Unknown),
-    Pin31: (pin31, 31, Unknown),
+    Pin0: (pin0, 0, GPIO0, Unknown),
+    Pin1: (pin1, 1, GPIO1, Unknown),
+    Pin2: (pin2, 2, GPIO2, Unknown),
+    Pin3: (pin3, 3, GPIO3, Unknown),
+    Pin4: (pin4, 4, GPIO4, Unknown),
+    Pin5: (pin5, 5, GPIO5, Unknown),
+    Pin6: (pin6, 6, GPIO6, Unknown),
+    Pin7: (pin7, 7, GPIO7, Unknown),
+    Pin8: (pin8, 8, GPIO8, Unknown),
+    Pin9: (pin9, 9, GPIO9, Unknown),
+    Pin10: (pin10, 10, GPIO10, Unknown),
+    Pin11: (pin11, 11, GPIO11, Unknown),
+    Pin12: (pin12, 12, GPIO12, Unknown),
+    Pin13: (pin13, 13, GPIO13, Unknown),
+    Pin14: (pin14, 14, GPIO14, Unknown),
+    Pin15: (pin15, 15, GPIO15, Unknown),
+    Pin16: (pin16, 16, GPIO16, Unknown),
+    Pin17: (pin17, 17, GPIO17, Unknown),
+    Pin18: (pin18, 18, GPIO18, Unknown),
+    Pin19: (pin19, 19, GPIO19, Unknown),
+    Pin20: (pin20, 20, GPIO20, Unknown),
+    Pin21: (pin21, 21, GPIO21, Unknown),
+    Pin22: (pin22, 22, GPIO22, Unknown),
+    Pin23: (pin23, 23, GPIO23, Unknown),
+    Pin24: (pin24, 24, GPIO24, Unknown),
+    Pin25: (pin25, 25, GPIO25, Unknown),
+    Pin26: (pin26, 26, GPIO26, Unknown),
+    Pin27: (pin27, 27, GPIO27, Unknown),
+    Pin28: (pin28, 28, GPIO28, Unknown),
+    Pin29: (pin29, 29, GPIO29, Unknown),
+    Pin30: (pin30, 30, GPIO30, Unknown),
+    Pin31: (pin31, 31, GPIO31, Unknown),
 ]);
